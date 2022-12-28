0bc5fea338dc2546cb2508adb6ef2cc2c0be942b
Rafael <rafaeldealmeida.192001@gmail.com>
2022-12-28 01:36:35 -0300

    Configurado Load Balancer e autoscaling

--- infra/main.tf
+++ infra/main.tf
@@ -33,7 +33,7 @@ resource "aws_key_pair" "chaveSSH"{
 }
 
 resource "aws_autoscaling_group" "grupo" {
-  availability_zones = ["${var.regiao_aws}a"]
+  availability_zones = ["${var.regiao_aws}a", "${var.regiao_aws}b"] //zona de disponibilidade
   name = var.nomeGrupo
   max_size = var.maximo
   min_size = var.minimo
@@ -41,4 +41,41 @@ resource "aws_autoscaling_group" "grupo" {
     id = aws_launch_template.maquina.id
     version = "$Latest"
   }
+  target_group_arns = [ aws_lb_target_group.alvoLoadBalancer.arn ]
   }
+
+//redes internas na aws
+  resource "aws_default_subnet" "subnet_1" { 
+    availability_zone = "${var.regiao_aws}a" 
+  }
+
+  resource "aws_default_subnet" "subnet_2" {
+    availability_zone = "${var.regiao_aws}b" 
+  }
+
+//Distribuidor de carga
+  resource "aws_lb" "loadBalancerProducao" {
+    internal = false
+    subnets = [aws_default_subnet.subnet_1.id,aws_default_subnet.subnet_2.id ]
+  }
+
+  resource "aws_default_vpc" "default" {
+
+  }
+
+  resource "aws_lb_target_group" "alvoLoadBalancer" {
+    name     = "maquinaAlvo"
+    port     = "8000"
+    protocol = "HTTP"
+    vpc_id   =  aws_default_vpc.default.id //Virtual Private Cloud
+  }
+
+resource "aws_listener" "entradaLoadBalancer" {
+  load_balancer_arn = aws_lb.load_balancer_arn
+  port = "8000"
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.alvoLoadBalancer.arn
+  }
+}
